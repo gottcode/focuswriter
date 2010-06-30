81e2609bc9f077adfae812533c7de6479304fe8a
Graeme Gott <graeme@gottcode.org>
2010-06-30 11:43:07 -0400

    Improve deploy scripts.

--- mac_deploy.sh
+++ mac_deploy.sh
@@ -4,11 +4,35 @@ APP='FocusWriter'
 BUNDLE="$APP.app"
 VERSION='1.2.1'
 
-DICTIONARIES="$BUNDLE/Contents/Resources/Dictionaries"
+# Create disk folder
+echo -n 'Copying application bundle... '
+rm -f "${APP}_$VERSION.dmg"
+rm -rf "$APP"
+mkdir "$APP"
+cp -Rpf README "$APP/"
+cp -Rpf "$BUNDLE" "$APP/"
+echo 'Done'
+
+# Copy dictionary
+echo -n 'Copying English dictionary... '
+DICTIONARIES="$APP/$BUNDLE/Contents/Resources/Dictionaries"
 mkdir $DICTIONARIES
 cp -f hunspell/en_US/en_US.aff $DICTIONARIES
 cp -f hunspell/en_US/en_US.dic $DICTIONARIES
-echo 'Copied English dictionary'
+echo 'Done'
 
-macdeployqt $BUNDLE -dmg
-mv "$APP.dmg" "${APP}_$VERSION.dmg"
+# Copy Qt frameworks
+echo -n 'Copying Qt frameworks and plugins... '
+macdeployqt "$APP/$BUNDLE"
+echo 'Done'
+
+# Create disk image
+echo -n 'Creating disk image... '
+hdiutil create -quiet -ov -srcfolder "$APP" -format UDBZ -volname "$APP" "${APP}_$VERSION.dmg"
+hdiutil internet-enable -quiet -yes "${APP}_$VERSION.dmg"
+echo 'Done'
+
+# Clean up disk folder
+echo -n 'Cleaning up... '
+rm -rf "$APP"
+echo 'Done'
--- windows_deploy.bat
+++ windows_deploy.bat
@@ -1,22 +1,40 @@
 @ECHO OFF
+
+SET APP=FocusWriter
+SET VERSION=1.2.1
+
 ECHO Copying executable
-MKDIR FocusWriter
-COPY release\FocusWriter.exe FocusWriter
-STRIP FocusWriter\FocusWriter.exe
-ECHO Copying libraries
-COPY %QTDIR%\bin\libgcc_s_dw2-1.dll FocusWriter
-COPY %QTDIR%\bin\mingwm10.dll FocusWriter
-COPY %QTDIR%\bin\QtCore4.dll FocusWriter
-COPY %QTDIR%\bin\QtGui4.dll FocusWriter
-ECHO Copying image plugins
-MKDIR FocusWriter\imageformats
-COPY %QTDIR%\plugins\imageformats\qgif4.dll FocusWriter\imageformats
-COPY %QTDIR%\plugins\imageformats\qico4.dll FocusWriter\imageformats
-COPY %QTDIR%\plugins\imageformats\qjpeg4.dll FocusWriter\imageformats
-COPY %QTDIR%\plugins\imageformats\qmng4.dll FocusWriter\imageformats
-COPY %QTDIR%\plugins\imageformats\qsvg4.dll FocusWriter\imageformats
-COPY %QTDIR%\plugins\imageformats\qtiff4.dll FocusWriter\imageformats
+MKDIR %APP%
+COPY README %APP%\README.txt >nul
+COPY release\%APP%.exe %APP% >nul
+strip %APP%\%APP%.exe
+
 ECHO Copying English dictionary
-MKDIR FocusWriter\Dictionaries
-COPY hunspell\en_US\en_US.aff FocusWriter\Dictionaries
-COPY hunspell\en_US\en_US.dic FocusWriter\Dictionaries
+SET DICTIONARIES=%APP%\Dictionaries
+MKDIR %DICTIONARIES%
+COPY hunspell\en_US\en_US.aff %DICTIONARIES% >nul
+COPY hunspell\en_US\en_US.dic %DICTIONARIES% >nul
+
+ECHO Copying Qt libraries
+COPY %QTDIR%\bin\libgcc_s_dw2-1.dll %APP% >nul
+COPY %QTDIR%\bin\mingwm10.dll %APP% >nul
+COPY %QTDIR%\bin\QtCore4.dll %APP% >nul
+COPY %QTDIR%\bin\QtGui4.dll %APP% >nul
+
+ECHO Copying Qt image plugins
+MKDIR %APP%\imageformats
+COPY %QTDIR%\plugins\imageformats\qgif4.dll %APP%\imageformats >nul
+COPY %QTDIR%\plugins\imageformats\qico4.dll %APP%\imageformats >nul
+COPY %QTDIR%\plugins\imageformats\qjpeg4.dll %APP%\imageformats >nul
+COPY %QTDIR%\plugins\imageformats\qmng4.dll %APP%\imageformats >nul
+COPY %QTDIR%\plugins\imageformats\qsvg4.dll %APP%\imageformats >nul
+COPY %QTDIR%\plugins\imageformats\qtiff4.dll %APP%\imageformats >nul
+
+ECHO Creating compressed file
+CD %APP%
+7z a %APP%_%VERSION%.zip * >nul
+CD ..
+MOVE %APP%\%APP%_%VERSION%.zip . >nul
+
+ECHO Cleaning up
+RMDIR /S /Q %APP%
