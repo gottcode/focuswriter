e0c0b2e3421c048d320061ba67e9a9c856482f95
Graeme Gott <graeme@gottcode.org>
2011-05-30 21:21:44 -0400

    FIXED: Regression where text did not fill text area.

--- src/document.cpp
+++ src/document.cpp
@@ -111,6 +111,7 @@ Document::Document(const QString& filename, int& current_wordcount, int& current
 	m_text->setMouseTracking(true);
 	m_text->setFrameStyle(QFrame::NoFrame);
 	m_text->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	m_text->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	m_text->setTabStopWidth(50);
 	m_text->document()->setIndentWidth(50);
 	m_text->viewport()->setMouseTracking(true);
