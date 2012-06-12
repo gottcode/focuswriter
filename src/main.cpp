7d656b93f66964a8c52b6ae8791b89197cd5f990
Graeme Gott <graeme@gottcode.org>
2012-06-12 11:31:52 -0400

    Tag version 1.3.6

--- src/main.cpp
+++ src/main.cpp
@@ -64,7 +64,7 @@ Application::Application(int& argc, char** argv)
 	m_window(0)
 {
 	setApplicationName("FocusWriter");
-	setApplicationVersion("1.3.5.2");
+	setApplicationVersion("1.3.6");
 	setOrganizationDomain("gottcode.org");
 	setOrganizationName("GottCode");
 	{
--- windows_deploy.bat
+++ windows_deploy.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET APP=FocusWriter
-SET VERSION=1.3.5.2
+SET VERSION=1.3.6
 
 ECHO Copying executable
 MKDIR %APP%
