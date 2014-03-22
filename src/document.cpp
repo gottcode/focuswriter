3dfc635ca10d7cd147e14bbed90a6f18e0f2bacb
Graeme Gott <graeme@gottcode.org>
2014-03-22 10:16:16 -0400

    FIXED: Reloading a modified file did not reduce daily goal progress.

--- src/document.cpp
+++ src/document.cpp
@@ -460,6 +460,7 @@ void Document::reload(bool prompt)
 	m_text->setReadOnly(true);
 	disconnect(m_text->document(), SIGNAL(contentsChange(int,int,int)), this, SLOT(updateWordCount(int,int,int)));
 	disconnect(m_text->document(), SIGNAL(undoCommandAdded()), this, SLOT(undoCommandAdded()));
+	m_daily_progress->increaseWordCount(-wordCountDelta());
 	loadFile(m_filename, -1);
 	emit loadFinished();
 }
