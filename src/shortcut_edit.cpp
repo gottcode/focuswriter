04766fb2ad2a41e7afebfd0ca88d13403d9ede61
Graeme Gott <graeme@gottcode.org>
2014-05-15 14:08:24 -0400

    Use QKeySequenceEdit for editing shortcuts. Closes #122.

--- src/shortcut_edit.cpp
+++ src/shortcut_edit.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * Copyright (C) 2012 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2012, 2014 Graeme Gott <graeme@gottcode.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,16 +22,106 @@
 #include <QHBoxLayout>
 #include <QKeyEvent>
 #include <QLabel>
-#include <QLineEdit>
 #include <QPushButton>
+
+#if (QT_VERSION >= (QT_VERSION_CHECK(5,2,0)))
+#include <QKeySequenceEdit>
+#else
+
+#include <QLineEdit>
+
+class QKeySequenceEdit : public QLineEdit
+{
+public:
+	QKeySequenceEdit(QWidget* parent) :
+		QLineEdit(parent)
+	{
+	}
+
+	void clear()
+	{
+		setKeySequence(QKeySequence());
+	}
+
+	QKeySequence keySequence() const
+	{
+		return m_sequence;
+	}
+
+	void setKeySequence(const QKeySequence& sequence)
+	{
+		m_sequence = sequence;
+		setText(m_sequence.toString(QKeySequence::NativeText));
+	}
+
+protected:
+	void keyPressEvent(QKeyEvent* event);
+
+private:
+	QKeySequence m_sequence;
+};
+
+void QKeySequenceEdit::keyPressEvent(QKeyEvent* event)
+{
+	Qt::KeyboardModifiers modifiers = event->modifiers() & (Qt::ShiftModifier | Qt::ControlModifier | Qt::AltModifier | Qt::MetaModifier);
+	int key = event->key();
+
+	switch (key) {
+	// Don't do anything if they only press a modifier
+	case Qt::Key_Shift:
+	case Qt::Key_Control:
+	case Qt::Key_Meta:
+	case Qt::Key_Alt:
+		return;
+
+	// Clear on backspace unless modifier is used
+	case Qt::Key_Backspace:
+	case Qt::Key_Delete:
+		if (modifiers == Qt::NoModifier) {
+			clear();
+			return;
+		}
+		break;
+
+	// Allow tab to change focus
+	case Qt::Key_Tab:
+	case Qt::Key_Backtab:
+		return;
+
+	default:
+		break;
+	}
+
+	// Add modifiers; only allow shift if it is not required for key of shortcut
+	if (modifiers & Qt::ShiftModifier) {
+		QChar c = !event->text().isEmpty() ? event->text().at(0) : QChar();
+		if (!c.isPrint() || c.isLetterOrNumber() || c.isSpace()) {
+			key |= Qt::SHIFT;
+		}
+	}
+	if (modifiers & Qt::ControlModifier) {
+		key |= Qt::CTRL;
+	}
+	if (modifiers & Qt::MetaModifier) {
+		key |= Qt::META;
+	}
+	if (modifiers & Qt::AltModifier) {
+		key |= Qt::ALT;
+	}
+
+	// Change shortcut
+	setKeySequence(QKeySequence(key));
+}
+
+#endif
 
 //-----------------------------------------------------------------------------
 
 ShortcutEdit::ShortcutEdit(QWidget* parent) :
 	QWidget(parent)
 {
-	m_edit = new QLineEdit(this);
-	m_edit->installEventFilter(this);
+	m_edit = new QKeySequenceEdit(this);
+	connect(m_edit, SIGNAL(editingFinished()), this, SIGNAL(changed()));
 
 	QPushButton* clear_button = new QPushButton(tr("Clear"), this);
 	connect(clear_button, SIGNAL(clicked()), this, SLOT(clear()));
@@ -54,97 +144,23 @@ ShortcutEdit::ShortcutEdit(QWidget* parent) :
 
 QKeySequence ShortcutEdit::shortcut() const
 {
-	return m_shortcut;
-}
-
-//-----------------------------------------------------------------------------
-
-bool ShortcutEdit::eventFilter(QObject* watched, QEvent* event)
-{
-	if ((watched == m_edit) && (event->type() == QEvent::KeyPress)) {
-		QKeyEvent* key_event = static_cast<QKeyEvent*>(event);
-
-		Qt::KeyboardModifiers modifiers = key_event->modifiers() & (Qt::ShiftModifier | Qt::ControlModifier | Qt::AltModifier | Qt::MetaModifier);
-		int key = key_event->key();
-
-		switch (key) {
-		// Don't do anything if they only press a modifier
-		case Qt::Key_Shift:
-		case Qt::Key_Control:
-		case Qt::Key_Meta:
-		case Qt::Key_Alt:
-			return true;
-
-		// Clear on backspace unless modifier is used
-		case Qt::Key_Backspace:
-		case Qt::Key_Delete:
-			if (modifiers == Qt::NoModifier) {
-				clear();
-				return true;
-			}
-			break;
-
-		// Allow tab to change focus
-		case Qt::Key_Tab:
-		case Qt::Key_Backtab:
-			return false;
-
-		default:
-			break;
-		}
-
-		// Add modifiers; only allow shift if it is not required for key of shortcut
-		if (modifiers & Qt::ShiftModifier) {
-			QChar c = !key_event->text().isEmpty() ? key_event->text().at(0) : QChar();
-			if (!c.isPrint() || c.isLetterOrNumber() || c.isSpace()) {
-				key |= Qt::SHIFT;
-			}
-		}
-		if (modifiers & Qt::ControlModifier) {
-			key |= Qt::CTRL;
-		}
-		if (modifiers & Qt::MetaModifier) {
-			key |= Qt::META;
-		}
-		if (modifiers & Qt::AltModifier) {
-			key |= Qt::ALT;
-		}
-
-		// Change shortcut
-		m_shortcut = QKeySequence(key);
-		setText();
-		emit changed();
-
-		return true;
-	} else {
-		return QWidget::eventFilter(watched, event);
-	}
-}
-
-//-----------------------------------------------------------------------------
-
-void ShortcutEdit::setShortcut(const QKeySequence& shortcut)
-{
-	setShortcut(shortcut, QKeySequence());
-	m_reset_button->setHidden(true);
+	return m_edit->keySequence();
 }
 
 //-----------------------------------------------------------------------------
 
 void ShortcutEdit::setShortcut(const QKeySequence& shortcut, const QKeySequence& default_shortcut)
 {
-	m_shortcut = shortcut;
 	m_default_shortcut = default_shortcut;
-	m_reset_button->setHidden(false);
-	setText();
+	m_reset_button->setHidden(default_shortcut.isEmpty());
+	m_edit->setKeySequence(shortcut);
 }
 
 //-----------------------------------------------------------------------------
 
 void ShortcutEdit::clear()
 {
-	m_shortcut = QKeySequence();
-	setText();
+	m_edit->clear();
 	emit changed();
 }
 
@@ -152,16 +168,8 @@ void ShortcutEdit::clear()
 
 void ShortcutEdit::reset()
 {
-	m_shortcut = m_default_shortcut;
-	setText();
+	m_edit->setKeySequence(m_default_shortcut);
 	emit changed();
-}
-
-//-----------------------------------------------------------------------------
-
-void ShortcutEdit::setText()
-{
-	m_edit->setText(m_shortcut.toString(QKeySequence::NativeText));
 }
 
 //-----------------------------------------------------------------------------
