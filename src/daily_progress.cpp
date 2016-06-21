f740f5111dfd17022cdb803ae2680bd43ee28d16
Graeme Gott <graeme@gottcode.org>
2016-06-21 15:43:58 -0400

    Add missing header.

--- src/daily_progress.cpp
+++ src/daily_progress.cpp
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * Copyright (C) 2013, 2014, 2015 Graeme Gott <graeme@gottcode.org>
+ * Copyright (C) 2013, 2014, 2015, 2016 Graeme Gott <graeme@gottcode.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,6 +25,8 @@
 #include <QLocale>
 #include <QSettings>
 #include <QTimer>
+
+#include <cmath>
 
 //-----------------------------------------------------------------------------
 
