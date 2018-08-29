47c293d7c8517a586bfecbb5b0fe66458d80aa14
Graeme Gott <graeme@gottcode.org>
2018-08-29 07:35:24 -0400

    FIXED: Could not always save to remote drives.
    This reverts commit 67f8cdb9503595e57777901ffbae52c1a624196e.

--- src/document_writer.cpp
+++ src/document_writer.cpp
@@ -23,9 +23,16 @@
 #include "odt_writer.h"
 #include "rtf_writer.h"
 
-#include <QSaveFile>
+#include <QFile>
 #include <QTextDocument>
 #include <QTextStream>
+
+#if defined(Q_OS_UNIX)
+#include <unistd.h>
+#elif defined(Q_OS_WIN)
+#include <windows.h>
+#include <io.h>
+#endif
 
 //-----------------------------------------------------------------------------
 
@@ -54,8 +61,7 @@ bool DocumentWriter::write()
 
 	bool saved = false;
 
-	QSaveFile file(m_filename);
-	file.setDirectWriteFallback(true);
+	QFile file(m_filename);
 	if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
 		return false;
 	}
@@ -89,11 +95,17 @@ bool DocumentWriter::write()
 		saved = stream.status() == QTextStream::Ok;
 	}
 
-	if (saved) {
-		saved = file.commit();
-	} else {
-		file.cancelWriting();
-	}
+	saved &= file.flush();
+#if defined(Q_OS_UNIX)
+	int ret;
+	do {
+		ret = fsync(file.handle());
+	} while ((ret == -1) && (errno == EINTR));
+	saved &= (ret == 0);
+#elif defined(Q_OS_WIN)
+	saved &= (FlushFileBuffers(reinterpret_cast<HANDLE>(_get_osfhandle(file.handle()))) != 0);
+#endif
+	file.close();
 
 	return saved;
 }
