74fe507781eb3f4d78812e5f3fe116057f8c3566
Graeme Gott <graeme@gottcode.org>
2019-12-26 10:47:50 -0500

    Correct path in Windows deployment.

--- windows_deploy.bat
+++ windows_deploy.bat
@@ -7,7 +7,7 @@ FOR /f %%i IN ('git describe') DO SET VERSION=%%i
 
 ECHO Copying executable
 MKDIR %APP%
-COPY release\%APP%.exe %APP% >nul
+COPY %APP%.exe %APP% >nul
 strip %APP%\%APP%.exe
 
 ECHO Copying translations
