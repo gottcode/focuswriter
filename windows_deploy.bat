e089910551e5c42b079ff227c43b3b4313013a55
Graeme Gott <graeme@gottcode.org>
2020-02-19 13:35:42 -0500

    Update windows deployment for Qt 5.14

--- windows_deploy.bat
+++ windows_deploy.bat
@@ -8,7 +8,6 @@ FOR /f %%i IN ('git describe') DO SET VERSION=%%i
 ECHO Copying executable
 MKDIR %APP%
 COPY %APP%.exe %APP% >nul
-strip %APP%\%APP%.exe
 
 ECHO Copying translations
 SET TRANSLATIONS=%APP%\translations
@@ -39,7 +38,7 @@ MKDIR %THEMES%
 XCOPY /Q /S /Y resources\themes\* %THEMES% >nul
 
 ECHO Copying Qt
-%QTDIR%\bin\windeployqt.exe --verbose 0 --release --no-angle --no-opengl-sw %APP%\%APP%.exe
+%QTDIR%\bin\windeployqt.exe --verbose 0 --no-angle --no-opengl-sw %APP%\%APP%.exe
 RMDIR /S /Q %APP%\iconengines
 
 ECHO Creating ReadMe
