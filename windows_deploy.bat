4ebc67d65f6b81e205609fa6cbc0c97d4b5ef02f
data-man <dataman@tutanota.com>
2021-10-03 13:45:05 +0500

    Update symbols to version 14.0

--- windows_deploy.bat
+++ windows_deploy.bat
@@ -33,7 +33,7 @@ MKDIR %SOUNDS%
 COPY resources\sounds\* %SOUNDS% >nul
 
 ECHO Copying symbols
-COPY resources\symbols\symbols1300.dat %APP% >nul
+COPY resources\symbols\symbols1400.dat %APP% >nul
 
 ECHO Copying themes
 SET THEMES=%APP%\themes
