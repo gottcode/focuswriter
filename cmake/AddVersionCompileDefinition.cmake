0c642bec1e966b6e3f340d65e30558fb638010d9
Graeme Gott <graeme@gottcode.org>
2022-09-16 06:51:26 -0400

    FIXED: Missing version number

--- cmake/AddVersionCompileDefinition.cmake
+++ cmake/AddVersionCompileDefinition.cmake
@@ -3,9 +3,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 function(add_version_compile_definition versionstr_file versionstr_def)
-	# Use project's VERSION by default
-	set(versionstr ${PROJECT_VERSION})
-
 	find_package(Git QUIET)
 	if(Git_FOUND)
 		# Find git repository
@@ -38,6 +35,11 @@ function(add_version_compile_definition versionstr_file versionstr_def)
 				)
 			endif()
 		endif()
+	endif()
+
+	# Fall back to project's VERSION
+	if ("${versionstr}" STREQUAL "")
+		set(versionstr ${PROJECT_VERSION})
 	endif()
 
 	# Pass version as compile definition to file
