e721c3b5a8ffb48bd44102c3760a7e57542f8e23
Graeme Gott <graeme@gottcode.org>
2024-01-01 14:51:50 -0500

    Update copyright year

--- src/window.cpp
+++ src/window.cpp
@@ -861,7 +861,7 @@ void Window::aboutClicked()
 		"<p align='center'>%6<br/><small>%7</small></p>")
 		.arg(tr("FocusWriter"), QApplication::applicationVersion(),
 			tr("A simple fullscreen word processor"),
-			tr("Copyright &copy; 2008-%1 Graeme Gott").arg("2023"),
+			tr("Copyright &copy; 2008-%1 Graeme Gott").arg("2024"),
 			tr("Released under the <a href=%1>GPL 3</a> license").arg("\"http://www.gnu.org/licenses/gpl.html\""),
 			tr("Uses icons from the <a href=%1>Oxygen</a> icon theme").arg("\"http://www.oxygen-icons.org/\""),
 			tr("Used under the <a href=%1>LGPL 3</a> license").arg("\"http://www.gnu.org/licenses/lgpl.html\""))
