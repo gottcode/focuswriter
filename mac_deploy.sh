dc9a8b86b9d3858ba7056dfa440daba5a124fc29
Graeme Gott <graeme@gottcode.org>
2024-01-16 09:02:23 -0500

    Bump version number

--- mac_deploy.sh
+++ mac_deploy.sh
@@ -2,7 +2,7 @@
 
 APP='FocusWriter'
 BUNDLE="$APP.app"
-VERSION='1.8.5'
+VERSION='1.8.6'
 
 # Locate deployment script
 BIN_DIR=$(pwd)
--- windows_deploy.bat
+++ windows_deploy.bat
@@ -4,7 +4,7 @@
 
 SET SRCDIR=..\focuswriter
 SET APP=FocusWriter
-SET VERSION=1.8.5
+SET VERSION=1.8.6
 
 ECHO Copying executable
 MKDIR %SRCDIR%\%APP%
