dd74ed4559a141653a06e7984c1251b992925775
David Lyon <davidlyon1988@gmail.com>
2025-04-07 10:39:44 +0200

    Fix compile error with QString::arg and char32_t on newer Clang
    Casting `unicode` to `quint32` avoids a compile-time template resolution error
    that occurs on Clang 17 (Xcode 15+) with Qt 5.15.16. This change ensures compatibility
    without affecting older builds.

--- src/symbols_dialog.cpp
+++ src/symbols_dialog.cpp
@@ -377,7 +377,7 @@ void SymbolsDialog::symbolClicked(const QModelIndex& symbol)
 		m_symbol_preview->setSceneRect(m_symbol_preview_item->boundingRect());
 		m_symbol_name->setText(name);
 		m_symbol_name->setToolTip(name);
-		m_symbol_code->setText(QString("<tt>U+%1</tt>").arg(unicode, 4, 16, QLatin1Char('0')).toUpper());
+		m_symbol_code->setText(QString("<tt>U+%1</tt>").arg(static_cast<quint32>(unicode), 4, 16, QLatin1Char('0')).toUpper());
 		m_symbol_shortcut->setShortcut(ActionManager::instance()->shortcut(unicode));
 
 		// Select symbol in recent list, and clear any other selections
